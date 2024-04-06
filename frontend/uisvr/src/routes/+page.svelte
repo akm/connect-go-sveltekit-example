c7eb708f9e26d2027c194bda6e8009b72ec549bc
akm <akm2000@gmail.com>
2024-04-06 21:27:11 +0900

    :sparkles: Implement greeting page

--- frontend/uisvr/src/routes/+page.svelte
+++ frontend/uisvr/src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { createPromiseClient } from '@connectrpc/connect';
+	import { createConnectTransport } from '@connectrpc/connect-web';
+
+	// Import service definition that you want to connect to.
+	import { GreetService } from '../gen/greet/v1/greet_connect';
+
+	// The transport defines what type of endpoint we're hitting.
+	// In our example we'll be communicating with a Connect endpoint.
+	// If your endpoint only supports gRPC-web, make sure to use
+	// `createGrpcWebTransport` instead.
+	const transport = createConnectTransport({
+		baseUrl: 'http://localhost:8080'
+	});
+
+	// Here we make the client itself, combining the service
+	// definition with the transport.
+	const client = createPromiseClient(GreetService, transport);
+
+	let inputValue = '';
+
+	type Message = {
+		fromMe: boolean;
+		message: string;
+	};
+	let messages: Message[] = [];
+</script>
+
+{#each messages as message}
+	<div>{message.fromMe ? 'Name: ' : 'Greeting: '}{message.message}</div>
+{/each}
+
+<form
+	on:submit={async (e) => {
+		const response = await client.greet({ name: inputValue });
+		messages = [
+			...messages,
+			{ fromMe: true, message: inputValue },
+			{ fromMe: false, message: response.greeting }
+		];
+		inputValue = '';
+	}}
+>
+	<input bind:value={inputValue} />
+	<button type="submit">Send</button>
+</form>
